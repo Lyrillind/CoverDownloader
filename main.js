b689c4ab193dc373eb87bb00495a48758d150235
Mullvinn <Mullvinn@gmail.com>
2018-11-22 19:48:23 +0800

    embed cover instead of cover.jpg

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import FileSaver from 'file-saver';
 import { argv } from 'yargs';
 import axios from 'axios';
 import child_process from 'child_process';
@@ -10,6 +9,7 @@ import path from 'path';
 import process from 'process';
 
 const SORTED_DIR = 'SORTED';
+const COVER_NAME = 'cover';
 
 const AUDIO_EXT = ['.flac','.mp3','.aac','.ogg','.m4a','.wav','.ape','.aiff'];
 
@@ -36,8 +36,12 @@ function checkApiServer() {
         serviceProcess = child_process.spawn(NODE_BIN, [API_SERVICE_PATH], {
           detached: true,
         });
+        serviceProcess.stdout.on('data', data => {
+          if (data.toString().indexOf('server running @') >= 0) {
+            checkApiServer();
+          }
+        });
       }
-      checkApiServer();
     } else {
       if (!serviceProcess) return;
       serviceProcess.kill('SIGINT');
@@ -50,7 +54,8 @@ function handleSongs(songs) {
   songs.sort((a, b) => path.basename(a) - path.basename(b));
   songs.forEach(song => {
     const filename = path.basename(song, path.extname(song));
-    let artist = filename.split('-')[0].trim();
+    const title = filename.split('-').reverse()[0].trim();
+    let artist = filename.replace(title, '').replace(/\s?-\s?$/, '')[0].trim();
     retrieveSongInfo(filename).then((info) => {
       const albumId = info.al.id;
       artist = info.ar ? info.ar[0].name : artist;
@@ -58,7 +63,7 @@ function handleSongs(songs) {
     }).then((response) => {
       const { picUrl, name } = response.data.album;
       const targetDir = path.join(targetPath, SORTED_DIR, artist, name.replace(/\//g, '\\/'));
-      return organizeSong(song, picUrl, targetDir);
+      return organizeSong(song, { album: name, artist, title }, picUrl, targetDir);
     }).then(() => {
       count += 1;
       if (count >= songs.length) {
@@ -82,9 +87,9 @@ function retrieveSongInfo(name) {
   });
 }
 
-function organizeSong(song, picUrl, downloadDir) {
+function organizeSong(song, songInfo, picUrl, downloadDir) {
   fs.ensureDirSync(downloadDir);
-  const targetLocation = path.join(downloadDir, `cover${path.extname(picUrl)}`);
+  const targetLocation = path.join(downloadDir, `${COVER_NAME}${path.extname(picUrl)}`);
   return new Promise((resolve, reject) => {
     const downloadProcess = child_process.spawn('curl', [picUrl, '--output', targetLocation], {
       detached: true
@@ -94,21 +99,82 @@ function organizeSong(song, picUrl, downloadDir) {
         const stats = fs.statSync(targetLocation);
         if (stats.size > 0) {
           const filename = path.basename(song).trim();
-          fs.moveSync(song, path.join(downloadDir, filename.replace(/\s+-\s+/g, '-')), { overwrite: true });
-          console.log(colors.green('✓'), colors.gray(path.basename(song, path.extname(song))));
-          resolve();
+          const dest = path.join(downloadDir, filename.replace(/\s+-\s+/g, '-'));
+          fs.copySync(song, dest, { overwrite: true });
+          embedArtIntoSong(dest, songInfo, targetLocation, resolve);
           return;
         }
       }
       retry(song, picUrl, downloadDir);
     });
+  }).then(() => {
+    console.log(colors.green('✓'), colors.gray(path.basename(song, path.extname(song))));
   });
 }
 
 function retry(song, picUrl, downloadDir) {
-  const targetLocation = path.join(downloadDir, `cover${path.extname(picUrl)}`);
+  const targetLocation = path.join(downloadDir, `${COVER_NAME}${path.extname(picUrl)}`);
   if (fs.pathExistsSync(targetLocation)) {
     fs.removeSync(targetLocation);
   }
   organizeSong(song, picUrl, downloadDir);
+}
+
+function embedArtIntoSong(song, songInfo, coverPath, resolve) {
+  const extname = path.extname(song);
+  switch (extname) {
+    case '.flac': {
+      handleFLACFile(song, songInfo, coverPath, resolve);
+      break;
+    }
+    case '.mp3': {
+      handleMP3File(song, songInfo, coverPath, resolve);
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+function handleFLACFile(song, songInfo, coverPath, resolve) {
+  const readProcess = child_process.spawn('metaflac', ['--list', song]);
+  readProcess.stdout.on('data', (data) => {
+    const output = data.toString();
+    if (output.indexOf('type: 3 (Cover (front))') < 0) {
+      const embedProcess = child_process.spawn('metaflac', [
+        `--set-tag=TITLE=${songInfo.title}`,
+        `--set-tag=ALBUM=${songInfo.album}`,
+        `--set-tag=ARTIST=${songInfo.artist}`,
+        `--import-picture-from=${coverPath}`,
+        song
+      ]);
+      embedProcess.on('close', resolve);
+    }
+  });
+}
+
+function handleMP3File(song, songInfo, coverPath, resolve) {
+  const readProcess = child_process.spawn('id3v2', ['-l', song]);
+  readProcess.stdout.on('data', (data) => {
+    if (data.toString().indexOf('APIC (Attached picture)') < 0) {
+      const embedProcess = child_process.spawn('lame', [
+        `--ti`, coverPath,
+        song,
+      ]);
+      embedProcess.on('close exit disconnect error message', (outputInfo) => {
+        console.log(outputInfo.toString());
+        if (outputInfo.toString().indexOf('Writing LAME Tag...done') >= 0) {
+          child_process.spawn('id3v2', [
+            `-t`, `"${songInfo.title}"`,
+            `-a`, `"${songInfo.artist}"`,
+            `-A`, `"${songInfo.album}"`,
+            song,
+          ]);
+          fs.removeSync(song);
+          fs.moveSync(`${song}.mp3`, song, { overwrite: true });
+          resolve();
+        }
+      });
+    }
+  });
 }
